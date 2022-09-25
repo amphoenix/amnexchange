9aacea560aa63e3652e4e1cf6de859aada0f4ee9
Aman <kumara4@amdocs.com>
2022-09-25 14:04:36 +0530

    update files

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import logo from '../../images/logo.png';
+import Aman_Resorts_logo from '../../images/Aman_Resorts_logo.png';
 
 const Footer = () => (
   <div className='w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer'>
     <div className='w-full flex sm:flex-row flex-col justify-between items-center my-4'>
       <div className='flex flex-[0.5] justify-center items-center'>
-        <img src={logo} alt='logo' className='w-32' />
+        <img src={Aman_Resorts_logo} alt='logo' className='w-32' />
       </div>
       <div className='flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full'>
         <p className='text-white text-base text-center mx-2 cursor-pointer'>
@@ -29,14 +29,14 @@ const Footer = () => (
         Come join us and hear for the unexpected miracle
       </p>
       <p className='text-white text-sm text-center font-medium mt-2'>
-        info@kryptomastery.com
+        amnshrvstv@gmail.com
       </p>
     </div>
 
     <div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 ' />
 
     <div className='sm:w-[90%] w-full flex justify-between items-center mt-3'>
-      <p className='text-white text-left text-xs'>@kryptomastery2022</p>
+      <p className='text-white text-left text-xs'>K.A. 2022</p>
       <p className='text-white text-right text-xs'>All rights reserved</p>
     </div>
   </div>
