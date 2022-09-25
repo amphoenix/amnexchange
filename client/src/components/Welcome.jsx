7ed8d3d8b1b8595960266ca45bfdb6c094a32cfe
Aman <kumara4@amdocs.com>
2022-09-26 00:50:26 +0530

    final

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,8 +3,8 @@ import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 
-// import { TransactionContext } from '../context/TransactionContext';
-// import { shortenAddress } from '../utils/shortenAddress';
+import { TransactionContext } from '../context/TransactionContext';
+import { shortenAddress } from '../utils/shortenAddress';
 import { Loader } from '.';
 
 const companyCommonStyles =
@@ -22,6 +22,25 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
+  const {
+    currentAccount,
+    connectWallet,
+    handleChange,
+    sendTransaction,
+    formData,
+    isLoading,
+  } = useContext(TransactionContext);
+
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+
+    e.preventDefault();
+
+    if (!addressTo || !amount || !keyword || !message) return;
+
+    sendTransaction();
+  };
+  
   return (
     <div className='flex w-full justify-center items-center'>
       <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
@@ -33,10 +52,10 @@ const Welcome = () => {
             Explore the crypto world. Buy and sell cryptocurrencies easily on
             Krypto.
           </p>
-          {/* {!currentAccount && ( */}
+          {!currentAccount && (
             <button
               type='button'
-              // onClick={connectWallet}
+              onClick={connectWallet}
               className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
             >
               <AiFillPlayCircle className='text-white mr-2' />
@@ -44,7 +63,7 @@ const Welcome = () => {
                 Connect Wallet
               </p>
             </button>
-          {/* )} */}
+          )}
 
           <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
@@ -75,7 +94,7 @@ const Welcome = () => {
               </div>
               <div>
                 <p className='text-white font-light text-sm'>
-                  {/* {shortenAddress(currentAccount)} */}
+                  {shortenAddress(currentAccount)}
                 </p>
                 <p className='text-white font-semibold text-lg mt-1'>
                   Ethereum
@@ -88,40 +107,40 @@ const Welcome = () => {
               placeholder='Address To'
               name='addressTo'
               type='text'
-              // handleChange={handleChange}
+              handleChange={handleChange}
             />
             <Input
               placeholder='Amount (ETH)'
               name='amount'
               type='number'
-              // handleChange={handleChange}
+              handleChange={handleChange}
             />
             <Input
               placeholder='Keyword (Gif)'
               name='keyword'
               type='text'
-              // handleChange={handleChange}
+              handleChange={handleChange}
             />
             <Input
               placeholder='Enter Message'
               name='message'
               type='text'
-              // handleChange={handleChange}
+              handleChange={handleChange}
             />
 
             <div className='h-[1px] w-full bg-gray-400 my-2' />
 
-            {/* {isLoading ? (
+            {isLoading ? (
               <Loader />
-            ) : ( */}
+            ) : (
               <button
                 type='button'
-                // onClick={handleSubmit}
+                onClick={handleSubmit}
                 className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer'
               >
                 Send now
               </button>
-            {/* )} */}
+            )}
           </div>
         </div>
       </div>
